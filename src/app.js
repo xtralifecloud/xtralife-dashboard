5fcf3ae4d6b66a74f084704fe5eee1089e517d4f
chris <root@chris.combobreaker.xtralife.me-ams3>
2020-06-24 15:09:00 +0000

    fix coffee translation error in directive

--- src/app.js
+++ src/app.js
@@ -279,13 +279,13 @@ app.directive('dtabset', function () {
 		restrict: 'E',
 		replace: true,
 		transclude: true,
-		controller($scope) {
+		controller: function($scope) {
 			$scope.templateUrl = '';
 			const tabs = ($scope.tabs = []);
 			const controller = this;
 
 			this.selectTab = function (tab) {
-				angular.forEach(tabs, function (tab) {
+				tabs.forEach(function (tab) {
 					tab.selected = false;
 				});
 				tab.selected = true;
@@ -305,33 +305,36 @@ app.directive('dtabset', function () {
 		},
 		template: "<div class=\"row-fluid\"><div class=\"row-fluid\"><div class=\"nav nav-tabs\" ng-transclude></div></div><div class=\"row-fluid\"><ng-include src=\"templateUrl\"></ng-include></div></div>"
 	};
-}).directive('dtab', () => ({
-	restrict: 'E',
-	replace: true,
-	require: '^dtabset',
+});
+app.directive('dtab', function () {
+	return {
+		restrict: 'E',
+		replace: true,
+		require: '^dtabset',
 
-	scope: {
-		title: '@',
-		icon: '@',
-		templateUrl: '@'
-	},
+		scope: {
+			title: '@',
+			icon: '@',
+			templateUrl: '@'
+		},
 
-	link(scope, element, attrs, tabsetController) {
-		tabsetController.addTab(scope);
+		link: function(scope, element, attrs, tabsetController) {
+			tabsetController.addTab(scope);
 
-		scope.select = function () {
-			tabsetController.selectTab(scope);
-		};
+			scope.select = function () {
+				tabsetController.selectTab(scope);
+			};
 
-		scope.$watch('selected', function () {
-			if (scope.selected) {
-				tabsetController.setTabTemplate(scope.templateUrl);
-			}
-		});
-	},
+			scope.$watch('selected', function () {
+				if (scope.selected) {
+					tabsetController.setTabTemplate(scope.templateUrl);
+				}
+			});
+		},
 
-	template: "<li ng-class=\"{active: selected}\"><a href=\"\" ng-click=\"select()\"><i class='{{icon}}'></i> {{ title }}</a></li>"
-}));
+		template: "<li ng-class=\"{active: selected}\"><a href=\"\" ng-click=\"select()\"><i class='{{icon}}'></i> {{ title }}</a></li>"
+	}
+});
 
 app.run(function ($rootScope, $http, AuthService) {
 	$rootScope.message = "";
